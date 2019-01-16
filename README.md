c7954a7a8c939aa62c49a78077625e4895a14fff
Osvaldo Alvarado <osvaldo.alvarado.dev@gmail.com>
2019-01-16 17:24:35 -0300

    Upload Content

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Laravel-Countries
+# Laravel-Countries
+
+
+Here have a migration and seeder for include a list of countries in your laravel project.
+this list has forked from https://gist.github.com/brenes/1095110#file-paises-csv
+
+# How Use?
+
+ - git clone https://github.com/osvaldoalvaradodev/Laravel-Countries.git
+ - copy migration to migration folder of your project
+ - copy seeder to seeder folder of your project
+ - Migrate and seed
---
+++ migration/2018_10_20_140608_create_countrys_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCountrysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countrys', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('name', 191);
+            $table->string('name_spanish', 191);
+            $table->string('iso2');
+            $table->string('iso3');
+            $table->string('phone_code', 191);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('countrys');
+    }
+}
